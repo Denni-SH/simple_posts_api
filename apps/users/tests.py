709905a10b5dae055687c00d1d5c024b98b09227
Denis Shkitun <denis.sh@powercode.us>
2019-04-02 17:20:40 +0300

    wip: tests creating

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CreateUserTest(APITestCase):
+    def setUp(self):
+        self.data = {
+            'username': 'mike', 'email': 'Mike@gmail.com',
+            'last_name': 'Tyson', 'password':'1234'
+        }
+
+    def test_can_create_user(self):
+        response = self.client.post(reverse('register'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
